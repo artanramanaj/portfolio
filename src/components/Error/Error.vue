9b3366cf9d263bea1ade29d47100620ffc10841f
Artan <artan.rramanaj@gmail.com>
2023-03-21 22:54:33 +0100

    error 404 page message

--- src/components/Error/Error.vue
+++ src/components/Error/Error.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="flex flex-col items-center justify-start gap-4 text-white">
+  <div
+    class="container flex flex-col items-center justify-start gap-4 text-white text-center"
+  >
     <img src="/img/error.png" alt="" class="w-1/3" />
     <p>
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Feugiat nisl,
-      nibh neque ullamcorper velit. Non cursus sodales netus viverra neque mi
-      quisque commodo.
+      I'm sorry, the page you're looking for could not be found.It's possible
+      that the page has been deleted, moved, or renamed. You can try searching
+      for the page using the search bar above, or navigate to my homepage to
+      find what you're looking for.
     </p>
     <button
       class="p-4 border-primary-green border-[1px] rounded-lg hover:bg-primary-green"
